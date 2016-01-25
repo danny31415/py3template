e52c2ca194f545d27684286ff592cd13e98e3636
Danny Lynch <danny31415@gmail.com>
2016-01-24 22:06:02 -0800

    Inital Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Template repository for a python package with py.test tests that run on CirlceCI
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='py3template',
+      version='0.1',
+      description='A template package with py.test testing',
+      url='http://github.com/danny31415/py3template',
+      author='Danny Lynch',
+      author_email='danny31415@gmail.com',
+      license='MIT',
+      packages=['py3template'],
+      zip_safe=False)
+
