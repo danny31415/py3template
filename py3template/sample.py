8d47e4b06127d1c4520a44c08952b3d58846f8a2
Danny Lynch <danny31415@gmail.com>
2016-01-24 23:38:06 -0800

    Add coverage report Add simple class test

---
+++ py3template/sample.py
@@ -0,0 +1,7 @@
+
+class Sample():
+    def __init__(self, data):
+        self.data = data
+
+    def print_data(self):
+        print(self.data)
--- tests/test_py3template.py
+++ tests/test_py3template.py
@@ -1,5 +1,10 @@
-import py3template
+from py3template.sample import Sample
 
 
 def test_dummy():
     assert 1 == 1
+
+
+def test_sample():
+    s = Sample(1)
+    assert s.data == 1
